37f0bdc17bce9294d3eefc090e3eeae35caf365f
Ioannis Manganas <manganas@ioannis@gmail.com>
2022-03-18 13:14:20 +0100

    Major changes in data preprocessing and main python script, v2

--- case1_v2_im.py
+++ case1_v2_im.py
@@ -1,24 +1,26 @@
-import Helpers.DataPreprocessing as hdp
-import Helpers.ErrorMetrics
-from importlib.resources import path
-import seaborn as sns
-import matplotlib.pyplot as plt
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import ElasticNet
-from sklearn.ensemble import RandomForestRegressor
-import sklearn.preprocessing as preproc
-from sklearn.model_selection import train_test_split, KFold, GridSearchCV
-import pandas as pd
-import numpy as np
-from tabnanny import verbose
+from sklearn.tree import DecisionTreeRegressor
 from random import random
-from sklearn.pipeline import Pipeline
+from tabnanny import verbose
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split, KFold, GridSearchCV, TimeSeriesSplit
+import sklearn.preprocessing as preproc
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.neighbors import KNeighborsRegressor
+import matplotlib.pyplot as plt
+import seaborn as sns
+from importlib.resources import path
+import Helpers.ErrorMetrics
+import Helpers.DataPreprocessing as hdp
 import os
 import sys
 script_path = os.path.abspath('')
 sys.path.append(script_path)
 sys.path.append(os.path.join(script_path, 'Helpers'))
+
 sns.set()
+
 
 # Read data
 file_name = 'Realized Schedule 20210101-20220228.xlsx'
@@ -28,23 +30,27 @@ raw_data = pd.read_excel(file_name)
 raw_data.loc[:, ('AircraftType')] = raw_data.loc[:,
                                                  ('AircraftType')].astype(str)
 
-raw_attributes_used = ['ScheduleTime', "Airline", 'Destination',
-                       'FlightType', 'Sector', 'SeatCapacity']
+raw_attributes_used = [
+    'ScheduleTime',
+    "Airline",
+    'Destination',
+    'FlightType',
+    'Sector',
+]
 
-raw_categorical_attributes_used = [
-    'Airline', 'Destination', 'FlightType', 'Sector']
 
+input_data = raw_data[raw_attributes_used]
+transformer = hdp.DataTransformer(top=3, dummy_encode=True)
+transformer.fit(input_data)
 
-num_pipeline = Pipeline([
-    ('data_transformer', hdp.AtemporalEncodingFeaturesTransformer(
-        raw_data, raw_attributes_used, raw_categorical_attributes_used, 10))
-])
+X_transformed_df = transformer.transform(input_data)
 
-data = num_pipeline.fit_transform(raw_data)
-# X, y = data.values[:, :-1], data.values[:, -1]
-X_df = data.drop(['SeatCapacity'], axis=1)
-y_df = pd.DataFrame(raw_data.LoadFactor)
+# Be careful for actual passengers = 0
+X_df = X_transformed_df[raw_data.LoadFactor != 0]
+load_factor = raw_data[raw_data.LoadFactor != 0].LoadFactor
+seat_capacity = raw_data[raw_data.LoadFactor != 0].SeatCapacity
+y_df = pd.DataFrame(data=load_factor.values *
+                    seat_capacity.values, columns=['Number of Passengers'])
 
 X = X_df.values
-y = y_df.values
-seat_capacities = data.SeatCapacity.values
+y = y_df.values.ravel()
